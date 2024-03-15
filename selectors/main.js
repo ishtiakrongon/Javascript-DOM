8608571c0e6b6613084a1518c7c1a9b30ceec2b0
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2024-03-15 23:14:08 +0600

    updated

---
+++ selectors/main.js
@@ -0,0 +1,9 @@
+// Selecting Elements using DOM
+
+// getElementbyID()
+let msg = document.getElementById("message");
+console.log(msg);
+
+// getElementByName()
+let btn = document.getElementsByName("language");
+console.log(btn);
