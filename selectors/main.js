f87f0e8adb0a72830c83ae7d84dd11ea6047c3cb
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2024-03-16 22:06:49 +0600

    updated

--- selectors/main.js
+++ selectors/main.js
@@ -17,3 +17,15 @@ console.log(tagName);
 
 let className = document.getElementsByClassName("message");
 console.log(className);
+
+// The querySelector() method allows you to select the first element that
+// matches one or more CSS selectors.
+// it will select only the first element of a css class
+let qrySelect = document.querySelector(".message");
+console.log(qrySelect);
+
+
+// use querySelectorAll() to select all elements of a same css class
+let cont = document.getElementById("container");
+let selectAll = cont.querySelectorAll(".msg2");
+console.log(selectAll);
