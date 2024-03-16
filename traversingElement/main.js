ee96ec46b104c9a0a914f439043079b50cd45d5a
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2024-03-16 22:45:29 +0600

    updated

---
+++ traversingElement/main.js
@@ -0,0 +1,13 @@
+let msg = document.querySelector(".text");
+console.log(msg.parentNode);
+
+//-------------------------------------------
+
+let parent = document.querySelector(".parent");
+console.log(parent.firstElementChild);
+console.log(parent.childNodes);
+
+//-------------------------------------------
+
+let second = document.querySelector(".second");
+console.log(second.nextElementSibling);
